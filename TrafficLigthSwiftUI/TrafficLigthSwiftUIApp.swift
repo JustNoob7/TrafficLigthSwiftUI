e33fbe6d0cb0565a7650fbb0d4fe1fd8aeb3d94e
Yaroslav Boyko <Spectraldagger322@gmail.com>
2021-10-26 18:55:42 +0300

    Initial Commit

---
+++ TrafficLigthSwiftUI/TrafficLigthSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLigthSwiftUIApp.swift
+//  TrafficLigthSwiftUI
+//
+//  Created by Ярослав Бойко on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLigthSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
