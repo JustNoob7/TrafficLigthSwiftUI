4a6b822d64566501bfabfbce2aa438ccfc08b5e1
Yaroslav Boyko <Spectraldagger322@gmail.com>
2021-10-27 13:31:01 +0300

    Jobs done

---
+++ TrafficLigthSwiftUI/CircleView.swift
@@ -0,0 +1,27 @@
+//
+//  CircleView.swift
+//  TrafficLigthSwiftUI
+//
+//  Created by Ярослав Бойко on 26.10.2021.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    let color: Color
+    var opacity: Double
+    
+    var body: some View {
+        Circle()
+            .frame(width: 100, height: 100)
+            .foregroundColor(color)
+            .opacity(opacity)
+            .overlay(Circle().stroke(.white ,lineWidth: 3))
+    }
+}
+
+struct CircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleView(color: .blue, opacity: 1)
+    }
+}
--- TrafficLigthSwiftUI/ContentView.swift
+++ TrafficLigthSwiftUI/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let fullOpacity = 1.0
+    private let weakOpacity = 0.3
+    
+    @State private var buttonText = "START"
+    @State private var redCircle = CircleView(color: .red, opacity: 0.3)
+    @State private var yellowCircle = CircleView(color: .yellow, opacity: 0.3)
+    @State private var greenCircle = CircleView(color: .green, opacity: 0.3)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                redCircle
+                    .padding(.bottom, 10.0)
+                yellowCircle
+                    .padding(.bottom, 10.0)
+                greenCircle
+                
+                Spacer()
+                
+                Button {
+                    changeColor()
+                } label: {
+                    Text(buttonText)
+                        .font(.largeTitle)
+                }
+                .frame(width: 150, height: 50)
+                .background(Color.white)
+                .cornerRadius(10)
+            }
+            .padding(.bottom)
+            .padding(.top)
+        }
+    }
+    
+    private func changeColor() {
+        if buttonText == "START" {
+            buttonText = "NEXT"
+        }
+        
+        switch redCircle.opacity {
+        case yellowCircle.opacity:
+            redCircle.opacity = fullOpacity
+            greenCircle.opacity = weakOpacity
+        case greenCircle.opacity:
+            greenCircle.opacity = fullOpacity
+            yellowCircle.opacity = weakOpacity
+        default:
+            yellowCircle.opacity = fullOpacity
+            redCircle.opacity = weakOpacity
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
